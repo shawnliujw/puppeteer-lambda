ee876f62eea9084e254565625708de245bdd554a
Liu Jianwei <liujianwei@patsnap.com>
2019-02-11 11:06:53 +0800

    add message when close browser

--- test/src/setup.test.js
+++ test/src/setup.test.js
@@ -2,18 +2,18 @@
  * Created by shawn-liu on 18/1/21.
  */
 const puppeteer = require('../../src/index');
-
+const Promise = require('bluebird');
 // describe('puppeteer setup test', () => {
 //     it('should get browser', () => {
         (async () => {
             const browser = await puppeteer.getBrowser({
-                dumpio: true,
+                dumpio: false,
                 headless: false
             });
             const page = await browser.newPage();
-            await page.goto('https://www.google.com');
+            await page.goto('https://www.facebook.com');
             await page.screenshot({path: 'example.png'});
-
+            await Promise.delay(2000)
             await browser.close();
         })();
 //     });

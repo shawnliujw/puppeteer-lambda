7ff5e0abaf312bdb98db4ba7e21ef15d53437f68
shawn <shawn@shawndeMacBook-Pro.local>
2019-06-21 17:33:19 +0800

    update scripts

--- packages/chromium/docker-build.sh
+++ packages/chromium/docker-build.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-rm -rf dist && mkdir -p dist
+rm -rf dist && mkdir -p dist/
 
-export CHROMIUM_VERSION=$(latest.sh stable)
-echo $CHROMIUM_VERSION
-
-echo "building image..."
-docker build -t headless-chromium:$CHROMIUM_VERSION --build-arg VERSION="$CHROMIUM_VERSION" .
-echo "extracting shell from image..."
-
-
-docker run --rm --entrypoint /bin/sh headless-chromium:$CHROMIUM_VERSION -c "cat build/chromium/src/out/Headless/headless_shell" > dist/headless_shell
+docker run -d -v dist/:/build amazonlinux:2 bash

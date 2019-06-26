2a717357718b3e6269d26b868b07d42f2accc8ee
Shawn Liu <shawn.jw.liu@gmail.com>
2019-06-26 11:59:33 +0800

    update readme refer to the nodejs10.x lambda

--- packages/chromium/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export CHROMIUM_VERSION=$(./latest.sh stable)
-echo $CHROMIUM_VERSION
-
-docker run -it -v $PWD/:/volume_chrome  -e VERSION=$CHROMIUM_VERSION   amazonlinux:2 bash
--- packages/chromium/ubuntu-script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-apt-get update && apt install -y docker.io apt-transport-https curl&& systemctl enable docker
-apt-get install -y jq
-
-git clone https://github.com/shawnliujw/puppeteer-lambda.git
-cd puppeteer-lambda && git checkout -b build remotes/origin/amazonlinux201803
-
-
-cd ~/puppeteer-lambda/packages/chromium && sh docker-build.sh
